c9b78a3d44b656d438f1ac7b87c56dbe10e86a60
Yamin Moulay <yaminmoulay12@gmail.com>
2025-02-12 11:36:56 +0100

    added routing, 404 page and artistpage, need fixing id url

--- rbx_react/src/components/navbar/Navbar.tsx
+++ rbx_react/src/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import classes from "./Navbar.module.scss";
 import data from "../../artists.json"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  // State pour afficher/masquer les images
   const [showImages, setShowImages] = useState(false);
   
 
-  // Données d'exemple des artistes (vous pouvez remplacer par un fetch API plus tard)
+  // Données des artistes (API plus tard)
   const artists = data;
   const handleMenuClick = () => {
     setShowImages(!showImages); // Toggle pour afficher/masquer les images
@@ -28,6 +30,7 @@ export default function Navbar() {
           }`}
         >
           {artists.map((artist) => (
+            <Link to={`/artist/${artist._id}`} key={artist.name} className={classes.artistLink}>
             <div key={artist.name} className={classes.artistCard}>
               <img
                 src={artist.thumbnail}
@@ -36,13 +39,14 @@ export default function Navbar() {
               />
               <p className={classes.artistName}>{artist.name.toUpperCase()}</p>
             </div>
+            </Link>
           ))}
         </div>
         {showImages && (<img src="./src/assets/close.png" alt="croix fermeture" onClick={handleMenuClick} className={classes.closeButton}/>)}
       </div>
       <div className={classes.caseVide}></div>
       <div className={classes.caseLink}>
-        <p>$</p>
+        <p>Rajouter un nom quand on clique sur l'artiste sélectionné</p>
       </div>
 
       
---
+++ rbx_react/src/pages/ArtistPage/ArtistPage.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbar/Navbar"
+
+export default function ArtistPage() {
+    return (
+        <>
+            <Navbar />
+            <p>page artiste</p>
+        </>
+        
+    )
+}
