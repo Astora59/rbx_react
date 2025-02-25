893b8f62bec6d1eb0316e34d77d2a3f55f5080fd
Yamin Moulay <yaminmoulay12@gmail.com>
2025-02-25 17:58:42 +0100

    _

--- rbx_react/src/pages/ArtistPage/ArtistPage.tsx
+++ rbx_react/src/pages/ArtistPage/ArtistPage.tsx
@@ -1,10 +1,16 @@
 import Navbar from "../../components/navbar/Navbar"
+import data from "../../artists.json"
+import { useParams } from "react-router-dom"
 
 export default function ArtistPage() {
+    const { _id } = useParams();
+    console.log(_id)
     return (
         <>
             <Navbar />
             <p>page artiste</p>
+            <p>{_id}</p>
+            
         </>
         
     )
